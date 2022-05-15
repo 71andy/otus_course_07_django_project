a5041e87b63e34edd2d47c368deabe3a8a155c0c
Andrey Mozzhevilov <71andy@mail.ru>
2022-05-15 19:16:27 +0300

    Упорядочил INSTALLED_APPS

--- hasker/hasker/settings.py
+++ hasker/hasker/settings.py
@@ -24,13 +24,13 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
-    'hsite.apps.HsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hsite.apps.HsiteConfig',
 ]
 
 MIDDLEWARE = [
